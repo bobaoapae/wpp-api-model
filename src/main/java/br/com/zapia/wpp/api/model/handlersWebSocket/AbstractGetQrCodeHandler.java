38c6711571a59f0d2eb03d73cf500839082fab38
bobaoapae <jvborges@outlook.com>
2021-05-23 01:46:46 -0300

    Handler qrcode

---
+++ src/main/java/br/com/zapia/wpp/api/model/handlersWebSocket/AbstractGetQrCodeHandler.java
@@ -0,0 +1,9 @@
+package br.com.zapia.wpp.api.model.handlersWebSocket;
+
+public abstract class AbstractGetQrCodeHandler implements IHandlerWebSocket<Void> {
+
+    @Override
+    public Class<Void> getClassType() {
+        return Void.class;
+    }
+}
